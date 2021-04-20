f34f8d5c69c7daf942a0c260a47e5e52a0ec50e7
Byun Byung Moon <58552467+Byung-moon@users.noreply.github.com>
2021-04-20 22:43:46 +0900

    Add files via upload

---
+++ 0418/bj1236.py
@@ -0,0 +1,30 @@
+N, M = map(int, input().split(" "))
+
+array = []
+
+for x in range(N):
+    array.append(input())
+
+# Row Search
+NeedRow = len(array)
+for x in range(len(array)):
+    for y in range(len(array[0])):
+        if array[x][y] == 'X':
+            NeedRow -= 1
+            break
+# Column Search
+NeedColumn = len(array[0])
+for x in range(len(array[0])):
+    for y in range(len(array)):
+        if array[y][x] == 'X':
+            NeedColumn -= 1
+            break
+
+# print('NeedRow : %d' %NeedRow)
+# print('NeedColumn : %d' %NeedColumn)
+
+# print answer
+if (NeedRow >= NeedColumn):
+    print(NeedRow)
+else:
+    print(NeedColumn)
